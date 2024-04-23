a2142a0e3e7421c117ed54c76642cf66668e3d96
Ammar Kashif <ammarkashif00@gmail.com>
2024-04-23 23:23:36 +0500

    Updated Producer to read the data in chunks using iJSON

--- producer.py
+++ producer.py
@@ -1,25 +1,41 @@
 from kafka import KafkaProducer
 import json
+import ijson
 import time
 
-def produce_dataset(file_path, producer, topics, num_objects=20):
+def produce_dataset(file_path, producer, topics, batch_size=20):
     try:
         with open(file_path, "r") as file:
-            data = json.load(file)
-            total_objects = len(data)
-            for i in range(0, total_objects, num_objects):
-                # Send chunks of num_objects each time
-                chunk = data[i:i+num_objects]
-                chunk_data = json.dumps(chunk).encode('utf-8')
+            # data = json.load(file)
+            # total_objects = len(data)
+            objects = ijson.items(file, 'item')  # Assuming the JSON array is the root element
+            batch = []
+            for obj in objects:
+                batch.append(obj)
+                if len(batch) >= batch_size:
+                    for topic in topics:
+                        producer.send(topic, json.dumps(batch).encode('utf-8'))
+                        producer.flush()
+                    batch = []
+                    print(f"Sent {batch_size} objects to {topics}")
+                    time.sleep(2)
+            if batch:  # Send the last partial batch if there is any
                 for topic in topics:
-                    producer.send(topic, chunk_data)
-                    producer.flush()  # Ensure data is sent immediately
-                    print(f"Sent {num_objects} objects to {topic}")
-                time.sleep(1)  # Sleep to simulate time delay between sends
-                if i + num_objects >= total_objects:
-                    print("End of dataset reached.")
-                    break
-            print("Dataset sent successfully")
+                    producer.send(topic, json.dumps(batch).encode('utf-8'))
+                    producer.flush()
+            # for i in range(0, total_objects, num_objects):
+            #     # Send chunks of num_objects each time
+            #     chunk = data[i:i+num_objects]
+            #     chunk_data = json.dumps(chunk).encode('utf-8')
+            #     for topic in topics:
+            #         producer.send(topic, chunk_data)
+            #         producer.flush()  # Ensure data is sent immediately
+            #         print(f"Sent {num_objects} objects to {topic}")
+            #     time.sleep(1)  # Sleep to simulate time delay between sends
+            #     if i + num_objects >= total_objects:
+            #         print("End of dataset reached.")
+            #         break
+            # print("Dataset sent successfully")
     except FileNotFoundError:
         print(f"File '{file_path}' not found.")
     except Exception as e:
@@ -34,10 +50,10 @@ topics = ['Apriori', 'PCY', 'Custom']
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
 # Path to the dataset file
-dataset_file = 'data/preprocessed_dataset_test.json'
+dataset_file = 'data/preprocessed_dataset.json'
 
 # Infinite loop to continuously send the dataset
 while True:
     produce_dataset(dataset_file, producer, topics)
 
-    time.sleep(2)  # Adjust the sleep duration as needed
+    time.sleep(1)  # Adjust the sleep duration as needed
